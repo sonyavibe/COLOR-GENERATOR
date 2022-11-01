a3add6cb273a8fca32099fa2f3340a9cacf47051
sofya vibe <105482011+sonyavibe@users.noreply.github.com>
2022-11-01 11:11:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,14 @@ const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 
 btn.addEventListener('click', function(){
-  let randomNumber = getRandomNumber();
-  
-  if (randomNumer <= 9) {
-    console.log(randomNumber);
-    document.body.style.backgroundColor = colors[randomNumber];
-    color.textContent = colors[randomNumber];
-  } else {
-    document.body.style.backgroundColor = colors[9];
-    color.textContent = colors[9];
-  };
-  
-});
+  const randomNumber = getRandomNumber();
+  console.log(randomNumber);
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+})
 
 function getRandomNumber() {
-  return Math.floor(Math.random() * (colors.length));
+  let result = Math.floor(Math.random() * (colors.length));
+  if (result >=10) result = 9;
+  return result;
 }
