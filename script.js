669bea02cd75a75af807c7384fe1ed5042c45123
sofya vibe <105482011+sonyavibe@users.noreply.github.com>
2022-11-01 10:58:40 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+const colors = ['#9747FF', '#5500C2', '#2036FF', '#000E8B', '#49FF51', '#00E392','#D0FF12', '#F1F1F1', '#D9D9D9', '#D1FF50'];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function(){
+  const randomNumber = getRandomNumber();
+  console.log(randomNumber);
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+})
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * (colors.length+1));
+}
