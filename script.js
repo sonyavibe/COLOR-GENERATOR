d5d0beea5af42c3d7da791725f85c84ee3e57815
sofya vibe <105482011+sonyavibe@users.noreply.github.com>
2022-11-01 11:03:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const color = document.querySelector('.color');
 btn.addEventListener('click', function(){
   const randomNumber = getRandomNumber();
   console.log(randomNumber);
+  if (randomNumer == 10) {
+    randomNumber = 9;
+  };
   document.body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
 })
